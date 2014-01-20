832d7d11af4bd60b04b375b270cce3e009b6dacc
Esteban Muñoz <edmunozg@gmail.com>
2014-01-19 23:35:38 -0500

    Changes in the class Browser

--- Browser/src/main/java/com/computernetworks/browser/App.java
+++ Browser/src/main/java/com/computernetworks/browser/App.java
@@ -1,13 +1,16 @@
 package com.computernetworks.browser;
 
+import guis.Browser;
+
 /**
  * Hello world!
  *
  */
 public class App 
-{
+{    
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        
     }
 }
