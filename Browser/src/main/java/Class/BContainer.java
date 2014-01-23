66c0b8cd13a4e2d86a4852cbf14476d4c0eee26a
Esteban Muñoz <edmunozg@gmail.com>
2014-01-23 02:20:44 -0500

    Wrapper class

---
+++ Browser/src/main/java/Class/BContainer.java
@@ -0,0 +1,41 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Class;
+
+import java.awt.*;
+import javax.swing.ImageIcon;
+
+/**
+ * BContainer class that inherits from a modal
+ * that allows fears of container the project.
+ * @author Esteban Muñoz
+ * @author Juan Mite
+ */
+public class BContainer extends Container{
+    private ImageIcon image; 
+    
+/**
+ * BContainer class that inherits from a modal
+ * that allows fears of container the project.
+ * @param nameImage location where's the image 
+ */
+    public BContainer(String nameImage){
+        this.image = new ImageIcon (nameImage);
+    }    
+    
+/**
+ * Method painting in the background of a JFrame an image.
+ * @param g object of type Graphics helping to get the coordinates for painting.
+ */
+    @Override
+    public void paint (Graphics g){
+        Rectangle r = g.getClipBounds();
+        g.setColor(this.getBackground());
+        g.fillRect (r.x, r.y, r.width, r.height);
+        g.drawImage (image.getImage(), 0, 0, this.getWidth(), this.getHeight(), this.getBackground(), this);       
+        super.paint(g);                        
+    } 
+    
+}
