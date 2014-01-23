85486bc7513c9f719041ab9c3a68c19c6c14dd74
Esteban Muñoz <edmunozg@gmail.com>
2014-01-23 02:22:08 -0500

    change in class GetterBrowser

--- Browser/src/main/java/Class/GetterBrowser.java
+++ Browser/src/main/java/Class/GetterBrowser.java
@@ -14,36 +14,46 @@ import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.io.Writer;
 import java.net.Socket;
+import javax.swing.JOptionPane;
 
+/**
+ * 
+ * @author Juan Mite
+ * @author Esteban Muñoz
+ */
 public class GetterBrowser{
     public String line="";
     public String html="";
     public String cookie="";
         
-   public String getHtml(String direccion) {
+/**
+ * 
+ * @param address
+ */
+   public String getHtml(String address) {
 
       Socket smtpSocket = null;
       OutputStream os = null;
       InputStream is = null;
-      String host = direccion;
+      String host = address;
 
       try {
-         // Crea socket con host y puerto 80
+         // Create socket to host on port 80
          smtpSocket = new Socket(host, 80);
 
-         // obtiene los canales
+         // Get the channels
          os = smtpSocket.getOutputStream();
          is = smtpSocket.getInputStream();
 
-      } catch (Exception e) {
-         
+      } catch (IOException e) {
+          JOptionPane.showMessageDialog(null, e.getMessage(), "Browser", JOptionPane.ERROR_MESSAGE);         
       }
 
-      // mira si fue creado el socket
+      // See if the socket was created
       if (smtpSocket != null && os != null && is != null) {
          try {
 
-            // define el protocolo de aplicacion - HTTP
+            // Defines the application protocol - HTTP.
 	            Writer writer = new OutputStreamWriter(smtpSocket.getOutputStream(), "ISO-8859-1");
 	            writer.write("GET / HTTP/1.1\n");
 	            writer.write("Host: " + host + "\n");
@@ -51,13 +61,13 @@ public class GetterBrowser{
 	            writer.write("User-Agent: Mozilla/5.0 (Windows; U; Windows NT 5.1; pt-BR; rv:1.8.1.14) Gecko/20080404 Firefox/2.0.0.14\n\n");
 	            writer.flush();
 	
-	            // Prepara la lectura de la pagina
+	            // Prepare reading page.
 	            BufferedReader reader = new BufferedReader(new InputStreamReader(is, "ISO-8859-1"));
 	
-	            // Va leyendo cada linea del response
+	            // It reads each line of the response.
 	            line = reader.readLine();
 	   
-	            while (reader.ready()) { // para cuando ya no se está recibiendo nada
+	            while (reader.ready()) { // For when it is not receiving anything.
 	                line = reader.readLine();
 	                html += line+"\n";
 	                if(line.startsWith("Set-Cookie:")){
@@ -67,43 +77,61 @@ public class GetterBrowser{
             }
              
             
-            // cierra los canales
+            // Closed channels.
             os.close();
             is.close();
             smtpSocket.close();
 
-         } catch (Exception e) {}
+         } catch (IOException e) {
+             JOptionPane.showMessageDialog(null, e.getMessage(), "Browser", JOptionPane.ERROR_MESSAGE);
+         }
       }
      
    return html;
    
    }
-
+   
+   
+   
+/**
+ * 
+ * @param host
+ * @param cookie
+ */
    public void writeCookie(String host, String cookie){
         File f;
         f = new File(host+".cookie");
-         //Escritura
+         // Writing.
         try{
         FileWriter w = new FileWriter(f);
         BufferedWriter bw = new BufferedWriter(w);
         PrintWriter wr = new PrintWriter(bw);  
-        wr.write(cookie);//escribimos en el archivo de las cookies
+        wr.write(cookie);// We write in the cookies file.
         wr.close();
         bw.close();
-        }catch(IOException e){}
-         }
-
+        }catch(IOException e){
+            JOptionPane.showMessageDialog(null, e.getMessage(), "Browser", JOptionPane.ERROR_MESSAGE);
+        }
+   }
+         
+/**
+ * 
+ * @param host
+ * @param html
+ */
     public void savePage(String host, String html){
         File f;
         f = new File(host+".html");
-         //Escritura 
+         // Writing.
         try{
 	        FileWriter w = new FileWriter(f);
 	        BufferedWriter bw = new BufferedWriter(w);
 	        PrintWriter wr = new PrintWriter(bw);  
-	        wr.write(html);//escribimos en el archivo html
+	        wr.write(html);// We write in the html file.
 	        wr.close();
 	        bw.close();
-        }catch(IOException e){}
+        }catch(IOException e){
+            JOptionPane.showMessageDialog(null, e.getMessage(), "Browser", JOptionPane.ERROR_MESSAGE);
+        }
      }
   }
